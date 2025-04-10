c6b26875125382e334dc6d9bc1394cc71a1a31b7
Nand Kishore <nandk@thinkdatalabs.com>
2025-04-10 11:08:57 +0530

    update the api

--- doctor/urls.py
+++ doctor/urls.py
@@ -31,14 +31,14 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="United Accreditation Foundation",
+        title="EMR Data Management",
         default_version="v1",
         description="REST implementation of Django authentication system. "
                     "djoser library provides a set of Django "
                     "Rest Framework views to handle basic actions such as "
                     "registration, login, logout, password reset "
                     "and account activation. It works with custom user model.",
-        contact=openapi.Contact(email="nandk@thinkdatalabs.com"),
+        contact=openapi.Contact(email="knand4930@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
