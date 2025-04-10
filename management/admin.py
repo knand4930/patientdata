e17582f3222d4838759f1b974afb3d0bce076ebf
Nand Kishore <nandk@thinkdatalabs.com>
2025-04-10 10:32:16 +0530

    complete admin panel

--- management/admin.py
+++ management/admin.py
@@ -11,6 +11,38 @@ class PatientCodingResource(resources.ModelResource):
     class Meta:
         model = PatientCoding
 
+class PatientHistoryResource(resources.ModelResource):
+    class Meta:
+        model = PatientHistory
+
+class PatientInformationResource(resources.ModelResource):
+    class Meta:
+        model = PatientInformation
+
+class PatientLabsResource(resources.ModelResource):
+    class Meta:
+        model = PatientLabs
+
+class PatientLDAResource(resources.ModelResource):
+    class Meta:
+        model = PatientLDA
+
+class PatientPostOPComplicationsResource(resources.ModelResource):
+    class Meta:
+        model = PatientPostOPComplications
+
+class PatientProcedureEventsResource(resources.ModelResource):
+    class Meta:
+        model = PatientProcedureEvents
+
+class PatientVisitResource(resources.ModelResource):
+    class Meta:
+        model = PatientVisit
+
+class PatientMedicationResource(resources.ModelResource):
+    class Meta:
+        model = PatientMedication
+
 
 class PatientCodingAdmin(ExportActionMixin, admin.ModelAdmin):
     resource_class = PatientCodingResource
@@ -20,35 +52,46 @@ class PatientCodingAdmin(ExportActionMixin, admin.ModelAdmin):
 admin.site.register(PatientCoding, PatientCodingAdmin)
 
 
-class PatientHistoryAdmin(admin.ModelAdmin):
+class PatientHistoryAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientCodingResource
+
     list_display = ("mrn", "diagnosis_code", "dx_name")
+    search_fields = ("mrn", "diagnosis_code", "dx_name")
 
 admin.site.register(PatientHistory, PatientHistoryAdmin)
 
-class PatientInformationAdmin(admin.ModelAdmin):
+class PatientInformationAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientInformationResource
     list_display = ("log_id", "mrn", "disch_disp_c", "disch_disp", "hosp_admsn_time", "hosp_disch_time", "los", "icu_admin_flag", "surgery_date")
 admin.site.register(PatientInformation, PatientInformationAdmin)
 
-class PatientLabsAdmin(admin.ModelAdmin):
+class PatientLabsAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientLabsResource
+
     list_display = ("log_id", "mrn","enc_type_nm", "lab_code","lab_name", "observation_value","measurement_units", "reference_range","abnormal_flag", "collection_datetime")
 admin.site.register(PatientLabs, PatientLabsAdmin)
 
-class PatientLDAAdmin(admin.ModelAdmin):
+class PatientLDAAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientLDAResource
     list_display = ("log_id", "mrn", "description", "properties_display", "site", "placement_instant", "removal_instant", "flo_meas_name", "line_group_name")
 admin.site.register(PatientLDA, PatientLDAAdmin)
 
-class PatientPostOPComplicationsAdmin(admin.ModelAdmin):
+class PatientPostOPComplicationsAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientPostOPComplicationsResource
     list_display = ("log_id", "mrn", "element_name", "context_name", "element_abbr", "smrtdta_elem_value")
 admin.site.register(PatientPostOPComplications, PatientPostOPComplicationsAdmin)
 
-class PatientProcedureEventsAdmin(admin.ModelAdmin):
+class PatientProcedureEventsAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientProcedureEventsResource
     list_display = ("log_id", "mrn", "event_display_name", "event_time", "note_text")
 admin.site.register(PatientProcedureEvents, PatientProcedureEventsAdmin)
 
-class PatientVisitAdmin(admin.ModelAdmin):
+class PatientVisitAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientVisitResource
     list_display = ("log_id", "mrn", "diagnosis_code", "dx_name")
 admin.site.register(PatientVisit, PatientVisitAdmin)
 
-class PatientMedicationAdmin(admin.ModelAdmin):
+class PatientMedicationAdmin(ExportActionMixin, admin.ModelAdmin):
+    resource_class = PatientMedicationResource
     list_display = ("log_id", "mrn", "ordering_date", "display_name")
 admin.site.register(PatientMedication, PatientMedicationAdmin)
