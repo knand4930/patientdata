e8ce37979c1e261998f919a7c12b9c2b3492d53d
Nand Kishore <nandk@thinkdatalabs.com>
2025-04-10 11:06:26 +0530

    done all over updation

--- doctor/settings.py
+++ doctor/settings.py
@@ -28,12 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g#1*)dpu6r1p!fw8zhjh*2t_jzs(*_na4xp8-++b&)p680_j@@'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
